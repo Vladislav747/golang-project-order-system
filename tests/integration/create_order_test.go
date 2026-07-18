5858d21901bc34d7cca22dcf33f5aab5dfb2db5a
Владислав Кудряков <vvkudriakov@edu.hse.ru>
2026-07-18 23:23:30 +0500

    added integration tests

--- tests/integration/create_order_test.go
+++ tests/integration/create_order_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/Vladislav747/golang-project-order-system/internal/service"
 )
 
-func TestCreateOrder_WithCheckDatabase(t *testing.T) {
+func TestCreateOrder_CheckEventsInDatabase(t *testing.T) {
 	pool := setupPostgres(t)
 	ctx := t.Context()
 	logger := zap.NewNop()
@@ -50,4 +50,58 @@ func TestCreateOrder_WithCheckDatabase(t *testing.T) {
 		}
 	}
 	require.True(t, found, "created event not found")
+}
+
+func TestUpdateOrder_CheckEventsInDatabase(t *testing.T) {
+	pool := setupPostgres(t)
+	ctx := t.Context()
+	logger := zap.NewNop()
+	svc := service.NewService(
+		repositoryOrder.NewRepository(pool, logger),
+		repositoryOrderEvent.NewRepository(pool, logger),
+		pool, // TxManager: у *pgxpool.Pool есть Begin
+		nil,
+		logger,
+	)
+	order := model.Order{
+		ID:          uuid.New(),
+		CustomerID:  uuid.New(),
+		Status:      "pending",
+		TotalAmount: 1000,
+		Currency:    "USD",
+		Items:       json.RawMessage(`[]`),
+	}
+	require.NoError(t, svc.CreateOrder(ctx, order))
+	updateOrder := model.Order{
+		ID:          order.ID,
+		CustomerID:  order.CustomerID,
+		Status:      "shipped",
+		TotalAmount: 1000,
+		Currency:    "USD",
+		Items:       json.RawMessage(`[]`),
+	}
+	require.NoError(t, svc.UpdateOrder(ctx, updateOrder))
+	got, err := svc.GetOrder(ctx, order.ID.String())
+	require.NoError(t, err)
+	require.Equal(t, order.ID, got.ID)
+	require.Equal(t, "shipped", got.Status)
+	events, err := svc.GetOrderEvents(ctx)
+	require.NoError(t, err)
+	require.NotEmpty(t, events)
+	var created, updated bool
+	for _, e := range events {
+		if e.OrderID != order.ID {
+			continue
+		}
+		switch e.EventType {
+		case model.EventCreated:
+			created = true
+			require.Equal(t, model.SourceHTTPSync, e.Source)
+		case model.EventUpdated:
+			updated = true
+			require.Equal(t, model.SourceHTTPSync, e.Source)
+		}
+	}
+	require.True(t, created, "created event not found")
+	require.True(t, updated, "updated event not found")
 }
